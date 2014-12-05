ef95d60bff1ad61212f838c332618c27ca6f1c22
Mohan K <mohankr03@gmail.com>
2014-12-05 11:23:46 -0600

    blob extract: refactored and consolidated
    - replaced cm extract-files.sh with echoes reads proprietary-blobs.txt
    - consolidated all prop blobs for oneplusone into proprietary-blobs.txt, cm had it split
      across other oppo devices.

--- extract-files.sh
+++ extract-files.sh
@@ -1,7 +1,69 @@
-#!/bin/sh
+#!/bin/bash
 
 set -e
 
-export VENDOR=oneplus
-export DEVICE=bacon
-./../../oppo/msm8974-common/extract-files.sh $@
+VENDOR=oneplus
+DEVICE=bacon
+DEVBASE=../../../vendor/$VENDOR/$DEVICE/proprietary
+echo "Deleting $DEVBASE"
+rm -rf $DEVBASE
+
+
+function extract() {
+  local _file=$1
+  local _basedir=$2
+  [ ! -f "$_file" ] && {
+    echo "[$_file] must be passed and exist"
+    return 1
+  }
+  [ ! -d "$_basedir" ] && {
+    mkdir -p $_basedir
+    return 1
+  }
+
+  local _optional=no
+  local _filepath=
+  local _dirpath=
+  local _filename=
+  OIFS="$IFS"
+  while IFS= read -r _line
+  do
+      _optional=no
+      if [[ $_line == \#* ]] || [[ "$_line" == "" ]]
+      then
+          continue
+      elif [[ $_line == -* ]]
+      then
+          _filepath=${_line:1}
+          _optional=yes
+      else
+          _filepath=${_line}
+      fi
+      OIFS2="$IFS"
+      IFS=":"
+      for _p in ${_filepath[@]}    # traverse through elements
+      do
+        local _relpath=${_p}
+        _filename="${_p##*/}"
+        if [[ $_p == *"/"* ]]
+        then
+          _dirpath="${_p%/*}"
+          if [ ! -d $_dirpath ]
+          then
+              mkdir -p "${_basedir}"/"${_dirpath}"
+          fi
+        else
+          _dirpath=""
+        fi
+        echo -n "Attempt to extract /system/${_relpath} -> ${_basedir}/${_relpath} "
+        adb pull /system/${_relpath} ${_basedir}/${_relpath} || \
+          echo " (SKIPPED)"
+      done
+  done < "$_file"
+  IFS="$OIFS"
+}
+
+
+
+extract proprietary-blobs.txt $DEVBASE
+
